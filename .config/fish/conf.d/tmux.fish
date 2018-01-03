016af0acf76fe45fc9ad846fc8112642091e8c5f
chanhun.jeong <chanhun.jeong@navercorp.com>
2018-01-03 17:46:08 +0900

    updated

--- .config/fish/conf.d/tmux.fish
+++ .config/fish/conf.d/tmux.fish
@@ -1,3 +1,11 @@
 function fs -d "Switch tmux session"
   tmux list-sessions -F "#{session_name}" | fzf | xargs tmux switch-client -t
 end
+
+function fc -d "Create new session"
+  tmux new-session -d -s $argv
+end
+
+function fd -d "Delete tmux session"
+  tmux list-sessions -F "#{session_name}" | fzf | xargs tmux kill-session -t
+end
