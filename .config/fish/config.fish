bb363d51f6e797baba51e140e1da231daa5a66fd
keyolk <chanhun.jeong@navercorp.com>
2018-09-01 10:54:39 +0900

    updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,6 +14,10 @@ alias grep='grep --color=always'
 alias less='less -R'
 alias watch='watch --color'
 alias ssh='/usr/bin/ssh -F ~/.ssh/config'
+alias du="ncdu --color dark -rr -x --exclude .git --exclude node_modules"
+alias help='tldr'
+alias preview="fzf --preview 'bat --color \"always\" {}'"
+alias cat='bat'
 
 ## common environment
 set -x EDITOR /usr/bin/nvim
@@ -24,12 +28,10 @@ set -x CLANG_LIB /usr/lib/libclang.so
 set -x CLANG_INCLUDE $CLANG_HOME/include 
 set -x GOPATH (readlink -f $HOME/work/gopath)
 set -x GIT_TERMINAL_PROMPT 1
+set -x FZF_DEFAULT_OPTS "--bind='ctrl-o:execute(code {})+abort'"
 
-## PATH
-set -x PATH $HOME/.local/bin $GOPATH/bin (gem environment gempath | cut -d: -f1)/bin $PATH
+set -U fish_user_paths $fish_user_paths $HOME/.local/bin $GOPATH/bin (gem environment gempath | cut -d: -f1)/bin
 
 # locale
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
-
-source ~/.local/bin/source.fish
